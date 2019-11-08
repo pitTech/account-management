6a8813e158d005c993cf36b973c3011a22055b31
pitTech <aonsercs@gmail.com>
2019-11-08 14:38:52 +0700

    Third

---
+++ src/components/Create.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>   
+        
+            Firstname
+            <b-form-input v-model="text1" placeholder="First Name"></b-form-input>
+            Lastname
+            <b-form-input v-model="text2" placeholder="Last Name"></b-form-input>
+            Age
+            <b-form-input type="number" v-model="text3" placeholder="Age"></b-form-input>
+            Birtday
+            <b-form-input  type="date" v-model="text1" placeholder="Email"></b-form-input>
+            <br>
+            <b-form-select v-model="selected" :options="options"></b-form-select>
+            <br>
+            <br>
+            <b-button variant="danger" type="submit">Add</b-button>
+            
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data(){
+            return{
+                selected: null,
+                options: [
+                { value: null, text: 'Gender' },
+                { value: 'a', text: 'Male' },
+                { value: 'b', text: 'Female' },
+                
+                ]
+            }
+        }
+       
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Tablelist.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="table">
+        <b-table striped hover :items="items"  >
+            <Tableitem/>
+        </b-table>
+    </div>
+</template>
+
+<script>
+import Tableitem from './Tableitem'
+    export default {
+        name:"table",
+         data() {
+      return {
+        items: [
+          { first_name: 'Dickerson', last_name: 'Macdonald', age: 40, Birtday:'11/08/2019', gender:'Male'  },
+        ]
+      }
+    },
+    components:{
+        Tableitem
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
